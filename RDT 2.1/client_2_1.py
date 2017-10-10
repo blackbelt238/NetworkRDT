db52d8e7a570c7a0f742d2535cc2d2c545a2c845
Ethan Peterson <cap2010.ep@gmail.com>
2017-10-10 17:30:45 -0600

    Initial commit. Rename files.

--- RDT 2.1/client_2_1.py
+++ RDT 2.1/client_2_1.py
@@ -13,17 +13,17 @@ if __name__ == '__main__':
             'A mathematician is a device for turning coffee into theorems. -- Paul Erdos',
             'Grove giveth and Gates taketh away. -- Bob Metcalfe (inventor of Ethernet) on the trend of hardware speedups not being able to keep up with software demands',
             'Wise men make proverbs, but fools repeat them. -- Samuel Palmer (1805-80)']
-    
-     
+
+
     timeout = 2 #send the next message if not response
     time_of_last_data = time.time()
-     
+
     rdt = RDT.RDT('client', args.server, args.port)
     for msg_S in msg_L:
         print('Converting: '+msg_S)
         rdt.rdt_1_0_send(msg_S)
-       
-        # try to receive message before timeout 
+
+        # try to receive message before timeout
         msg_S = None
         while msg_S == None:
             msg_S = rdt.rdt_1_0_receive()
@@ -33,9 +33,9 @@ if __name__ == '__main__':
                 else:
                     continue
         time_of_last_data = time.time()
-        
+
         #print the result
         if msg_S:
             print('to: '+msg_S+'\n')
-        
+
     rdt.disconnect()
--- RDT 2.1/rdt_2_1.py
+++ RDT 2.1/rdt_2_1.py
@@ -9,12 +9,12 @@ class Packet:
     seq_num_S_length = 10
     length_S_length = 10
     ## length of md5 checksum in hex
-    checksum_length = 32 
-        
+    checksum_length = 32
+
     def __init__(self, seq_num, msg_S):
         self.seq_num = seq_num
         self.msg_S = msg_S
-        
+
     @classmethod
     def from_byte_S(self, byte_S):
         if Packet.corrupt(byte_S):
@@ -23,8 +23,8 @@ class Packet:
         seq_num = int(byte_S[Packet.length_S_length : Packet.length_S_length+Packet.seq_num_S_length])
         msg_S = byte_S[Packet.length_S_length+Packet.seq_num_S_length+Packet.checksum_length :]
         return self(seq_num, msg_S)
-        
-        
+
+
     def get_byte_S(self):
         #convert sequence number of a byte field of seq_num_S_length bytes
         seq_num_S = str(self.seq_num).zfill(self.seq_num_S_length)
@@ -35,8 +35,8 @@ class Packet:
         checksum_S = checksum.hexdigest()
         #compile into a string
         return length_S + seq_num_S + checksum_S + self.msg_S
-   
-    
+
+
     @staticmethod
     def corrupt(byte_S):
         #extract the fields
@@ -44,31 +44,31 @@ class Packet:
         seq_num_S = byte_S[Packet.length_S_length : Packet.seq_num_S_length+Packet.seq_num_S_length]
         checksum_S = byte_S[Packet.seq_num_S_length+Packet.seq_num_S_length : Packet.seq_num_S_length+Packet.length_S_length+Packet.checksum_length]
         msg_S = byte_S[Packet.seq_num_S_length+Packet.seq_num_S_length+Packet.checksum_length :]
-        
+
         #compute the checksum locally
         checksum = hashlib.md5(str(length_S+seq_num_S+msg_S).encode('utf-8'))
         computed_checksum_S = checksum.hexdigest()
         #and check if the same
         return checksum_S != computed_checksum_S
-        
+
 
 class RDT:
     ## latest sequence number used in a packet
     seq_num = 1
     ## buffer of bytes read from network
-    byte_buffer = '' 
+    byte_buffer = ''
 
     def __init__(self, role_S, server_S, port):
         self.network = Network.NetworkLayer(role_S, server_S, port)
-    
+
     def disconnect(self):
         self.network.disconnect()
-        
+
     def rdt_1_0_send(self, msg_S):
         p = Packet(self.seq_num, msg_S)
         self.seq_num += 1
         self.network.udt_send(p.get_byte_S())
-        
+
     def rdt_1_0_receive(self):
         ret_S = None
         byte_S = self.network.udt_receive()
@@ -88,20 +88,21 @@ class RDT:
             #remove the packet bytes from the buffer
             self.byte_buffer = self.byte_buffer[length:]
             #if this was the last packet, will return on the next iteration
-            
-    
+
+    # sends a packet based on the RDT 2.1 protocol
     def rdt_2_1_send(self, msg_S):
         pass
-        
+
+    # recieves a packet based on the RDT 2.1 protocol    
     def rdt_2_1_receive(self):
         pass
-    
+
     def rdt_3_0_send(self, msg_S):
         pass
-        
+
     def rdt_3_0_receive(self):
         pass
-        
+
 
 if __name__ == '__main__':
     parser =  argparse.ArgumentParser(description='RDT implementation.')
@@ -109,22 +110,17 @@ if __name__ == '__main__':
     parser.add_argument('server', help='Server.')
     parser.add_argument('port', help='Port.', type=int)
     args = parser.parse_args()
-    
+
     rdt = RDT(args.role, args.server, args.port)
     if args.role == 'client':
         rdt.rdt_1_0_send('MSG_FROM_CLIENT')
         sleep(2)
         print(rdt.rdt_1_0_receive())
         rdt.disconnect()
-        
-        
+
+
     else:
         sleep(1)
         print(rdt.rdt_1_0_receive())
         rdt.rdt_1_0_send('MSG_FROM_SERVER')
         rdt.disconnect()
-        
-
-
-        
-        
